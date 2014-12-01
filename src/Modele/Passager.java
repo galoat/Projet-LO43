500f9876f116ca0797632a44617411fff078c022
Florian Lacour <flacour@pc-gi-115.utbm.fr>
2014-12-01 17:51:58 +0100

    changement du nom mission pour passager commencement de la classe boite au lettre et ajout package exception

---
+++ src/Modele/Passager.java
@@ -0,0 +1,16 @@
+package Modele;
+
+import java.util.ArrayList;
+
+public class Passager {
+private ArrayList <Boolean> trajet;
+private boolean authoriser;
+private  ArrayList <Integer> prioriter;
+private Mission maMission;
+public void setTrajet(){
+	
+};
+public void genererRequestMap(){
+	
+};
+}
