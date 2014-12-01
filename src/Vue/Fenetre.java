68a4dc7adad563a2480b0e993ba90e9cc8f89d56
redhelling21 <theo.buri@utbm.fr>
2014-12-01 20:01:58 +0100

    Mise a jour de certains fichiers non-commités
    Because of Git Cancer

--- src/Vue/Fenetre.java
+++ src/Vue/Fenetre.java
@@ -3,8 +3,11 @@ package Vue;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.net.URL;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,20 +20,55 @@ public class Fenetre extends JFrame implements Observer {
 	private Verificateur verif;
 	private JPanel mainpan, secondpan;
 	private JLabel titre;
-	private JButton Bcredits, Bmanu, Bauto, Bopt;
+	private JButton Bcredits, Bmanu, Bauto, Bopt, close, reduce;
+	private BarPanel barpan;
+	private String theme;
+	private boolean sound;
 
 	public Fenetre(Verificateur v) {
 		this.verif = v;
+		this.setUndecorated(true);
+		this.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
+		this.getContentPane().setLayout(new BorderLayout());
 		this.setSize(350, 500);
+		this.theme = "Dark";
+		this.sound=false;
 		this.setTitle("Road Simulator 2014");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
+		createBarPanel();
 		createCompos();
-		this.setContentPane(mainpan);
 		this.setVisible(true);
 	}
+	private void createBarPanel(){
+		barpan = new BarPanel(this);
+		barpan.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
+		barpan.setPreferredSize(new Dimension(350, 25));
+		barpan.setBorder(BorderFactory.createLineBorder(Color.black));
+		ImageIcon icon = createImageIcon("close.png", "");
+		ImageIcon icon2 = createImageIcon("reduce.png", "");
+		close = new JButton("");
+		reduce = new JButton("");
+		close.setIcon(icon);
+		reduce.setIcon(icon2);
+		barpan.add(reduce);
+		barpan.add(close);
+		
+		
+	}
+	
 
+	protected ImageIcon createImageIcon(String path, String description) {
+	 	URL imgURL = getClass().getResource(path);
+	 	if (imgURL != null) {
+			return new ImageIcon(imgURL, description);
+		} else {
+	 		System.err.println("Impossible de trouver: " + path);
+			return null;
+	 	}
+	}
+	 	
 	private void createCompos() {
 		mainpan = new JPanel();
 		secondpan = new JPanel();
@@ -44,9 +82,9 @@ public class Fenetre extends JFrame implements Observer {
 		secondpan.add(Bauto);
 		secondpan.add(Bopt);
 		secondpan.add(Bcredits);
-		mainpan.setLayout(new BorderLayout());
-		mainpan.add(titre, BorderLayout.NORTH);
-		mainpan.add(secondpan, BorderLayout.CENTER);
+		this.getContentPane().add(barpan, BorderLayout.NORTH);
+		this.getContentPane().add(titre, BorderLayout.CENTER);
+		this.getContentPane().add(secondpan, BorderLayout.SOUTH);
 		
 		
 	}
