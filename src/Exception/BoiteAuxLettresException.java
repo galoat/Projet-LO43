2ec0addae956aa13a978a331fe3857d9d156a95c
Florian Lacour <flacour@pc-gi-115.utbm.fr>
2014-12-01 18:40:01 +0100

    fin de la seance de Tp du 1/12/14

---
+++ src/Exception/BoiteAuxLettresException.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class BoiteAuxLettresException extends Exception{
+	/**
+	 * variable utiliser par le compilateur
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
+	 * Constructeur par default permet de remonter une erreur 
+	 */
+	public BoiteAuxLettresException(){
+		super();
+	}
+	}
