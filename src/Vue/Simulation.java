6ff72c29d3c0cee725c864abe5b084b20e1bf96c
Hellong <theo.buri@utbm.fr>
2014-12-07 16:43:16 +0100

    Coordonnées des places calculées + ressources

--- src/Vue/Simulation.java
+++ src/Vue/Simulation.java
@@ -7,6 +7,7 @@ import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.Box;
@@ -27,11 +28,14 @@ public class Simulation extends MainPan {
 	private JComboBox depart, arrivee;
 	private JLabel dep, ar;
 	private String[] tab = {"1", "2", "3", "4", "5", "6"};
+	private ArrayList<Place> places;
+	private int centerx = 158, centery = 179, r = 25, dist = 132;
 
 	
 
 	public Simulation(boolean auto, Fenetre mere) {
 		super();
+		buildPlaces();
 		mere.setSize(new Dimension(350, 475));
 		this.setPreferredSize(new Dimension(350, 450));
 		this.setLayout(new BorderLayout());
@@ -84,6 +88,17 @@ public class Simulation extends MainPan {
 		this.add(map, BorderLayout.CENTER);
 		this.add(buttonpan, BorderLayout.SOUTH);
 	}
+	
+	private void buildPlaces(){
+		places = new ArrayList<Place>();
+		places.add(new Place(0, centerx, centery));
+		places.add(new Place(1, (int)(centerx - (dist/2)*Math.sqrt(3)), (centery - (dist/2))));
+		places.add(new Place(2, centerx, centery - dist));
+		places.add(new Place(3, (int) (centerx + (dist/2)*Math.sqrt(3)), centery - (dist/2)));
+		places.add(new Place(4, (int) (centerx + (dist/2)*Math.sqrt(3)), centery + (dist/2)));
+		places.add(new Place(5, centerx, centery + dist));
+		places.add(new Place(6, (int) (centerx - (dist/2)*Math.sqrt(3))-r, centery - (dist/2)));
+	}
 
 	private class MapPan extends MainPan {
 		/**
@@ -91,15 +106,40 @@ public class Simulation extends MainPan {
 		 */
 
 		protected void paintComponent(Graphics g) {
+			int centerx = 158;
+			int centery = 179;
+			int r = 25;
+			int dist = 132;
 			try { // On dessine l'image(avec des couples de coordonnees pour
 					// chaque coin)
-				g.drawImage(ImageIO.read(this.getClass().getResource("Map_proj.png")), 0,
+				g.drawImage(ImageIO.read(this.getClass().getResource("Map_proj2.png")), 0,
 						0, this.getWidth(), this.getHeight(), 0, 0,
 						this.getWidth(), this.getHeight(), null);
+				g.setColor(new Color(90, 150, 12));
+				//Le centre de l'image se trouve en 157, 177
+				g.fillOval(centerx-r, centery-r, r, r);//CENTRE
+				g.fillOval(centerx-r, (centery-dist)-r, r, r);//2
+				g.fillOval((int) (centerx + (dist/2)*Math.sqrt(3))-r, (centery - (dist/2))-r, r, r);//3
+				g.fillOval((int) (centerx + (dist/2)*Math.sqrt(3))-r, (centery + (dist/2))-r, r, r);//4
+				g.fillOval(centerx-r, centery+dist-r, r, r);//5
+				g.fillOval((int) (centerx - (dist/2)*Math.sqrt(3))-r, (centery + (dist/2))-r, r, r);//6
+				g.fillOval((int) (centerx - (dist/2)*Math.sqrt(3))-r, (centery - (dist/2))-r, r, r);//1
 
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+		}
+	}
+	
+	public class Place{
+		
+		int iD;
+		int x;
+		int y;
+		public Place(int iD, int x, int y) {
+			this.iD = iD;
+			this.x = x;
+			this.y = y;
 		}
 	}
 }
